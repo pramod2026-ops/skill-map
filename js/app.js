78d0773733c42cf9ec2459bb65575ffe7363e647
Pramod Ghimire <275212426+pramod2026-ops@users.noreply.github.com>
2026-09-06 16:40:21 +0900

    Build dynaamic skill tracking and progress update

--- js/app.js
+++ js/app.js
@@ -4,34 +4,66 @@ const skills = [
   { name: "HTML", status: "done" },
   { name: "CSS", status: "done" },
   { name: "JavaScript", status: "learning" },
-  { name: "Git & GitHub", status: "learning" },
+  { name: "Git & GitHub", status: "not-started" },
   { name: "REST APIs", status: "not-started" },
-  { name: "HTML", status: "not-started" },
-  { name: "HTML", status: "not-started" },
-  { name: "HTML", status: "not-started" },
+  { name: "React", status: "not-started" },
+  { name: "TypeScript", status: "not-started" },
+  { name: "Next.js", status: "not-started" },
 ];
 
 const skillList = document.querySelector("#skill-list");
-
-skillList.innerHTML = "";
-
-skills.forEach(function (skill) {
-  const li = document.createElement("li");
-
-  li.textContent = skill.name + " - " + skill.status;
-  li.classList.add(skill.status);
-
-  skillList.appendChild(li);
-});
-
 const progressText = document.querySelector("#progress-text");
+const progressFill = document.querySelector("#progress-fill");
+const nextSkill = document.querySelector("#next-skill");
+const startLearningButton = document.querySelector("#startlearning");
 
-const completedSkills = skills.filter(function (skill) {
-  return skill.status === "done";
+function renderSkills() {
+  skillList.innerHTML = "";
+
+  skills.forEach(function (skill) {
+    const li = document.createElement("li");
+
+    li.textContent = skill.name + " - " + skill.status;
+    li.classList.add(skill.status);
+
+    skillList.appendChild(li);
+  });
+}
+
+function updateProgress() {
+  const completedSkills = skills.filter(function (skill) {
+    return skill.status === "done";
+  });
+
+  const progress = (completedSkills.length / skills.length) * 100;
+  progressText.textContent = "Progress:  " + progress + "%";
+  progressFill.style.width = progress + "%";
+}
+
+function updateNextSkill() {
+  const learningSkill = skills.find(function (skill) {
+    return skill.status === "learning";
+  });
+
+  if (learningSkill) {
+    nextSkill.textContent = learningSkill.name;
+  } else {
+    nextSkill.textContent = "All Skills completed!";
+  }
+}
+
+startLearningButton.addEventListener("click", function () {
+  const learningSkill = skills.find(function (skill) {
+    return skill.status === "learning";
+  });
+
+  if (learningSkill) {
+    learningSkill.status = "done";
+  }
+  renderSkills();
+  updateProgress();
 });
 
-const progress = (completedSkills.length / skills.length) * 100;
-progressText.textContent = "Progress:  " + progress + "%";
-
-const progressFill = document.querySelector("#progress-fill");
-progressFill.style.width = progress + "%";
+renderSkills();
+updateProgress();
+updateNextSkill();
